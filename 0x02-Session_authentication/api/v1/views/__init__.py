33d1be9a9ee9e94b616215b51752c66205f333e1
Hajar-Hafdi <hajarlakbir@outlook.com>
2025-05-29 22:00:55 -0700

    fixing errors

---
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -0,0 +1,4 @@
+# api/v1/views/__init__.py
+
+from api.v1.views.index import app_views
+
--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -2,9 +2,11 @@
 """Module of Index views.
 """
 from flask import jsonify, abort
-from api.v1.views import app_views
 
+# Create the blueprint object here
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Then define routes using app_views
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """GET /api/v1/status
